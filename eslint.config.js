3a2f073b047ec7bbbed79d2c4279aff9ed01ab88
Elena Khadasevich <camatsu90@mail.ru>
2025-05-01 23:20:11 +0300

    Исправил баг с формой авторизации

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,165 @@
 import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
+import eslintReact from 'eslint-plugin-react';
+import * as reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import tseslint, { configs, plugin } from 'typescript-eslint';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import { rules } from 'eslint-config-prettier';
+import * as pluginImportX from 'eslint-plugin-import-x';
+import * as tsParser from '@typescript-eslint/parser';
+import jsxAlly from 'eslint-plugin-jsx-a11y';
+import jest from 'eslint-plugin-jest';
+import testingLibrary from 'eslint-plugin-testing-library';
+import vitest from 'eslint-plugin-vitest';
 
-export default tseslint
-  .config(
-    { ignores: ['dist'] },
-    {
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
-      files: ['**/*.{ts,tsx}'],
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-        
+/** @type {import('eslint').Linter.Config} */
+export default tseslint.config(
+  {
+    ignores: ['dist/**/*.ts', 'dist/**', '**/*.js', 'node_modules'],
+  },
+  {
+    plugins: {
+      tseslint: plugin,
+      'react-hooks': reactHooks,
+      react: eslintReact,
+      'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier,
+      jest,
+      'testing-library': testingLibrary,
+      vitest,
+    },
+  },
+  js.configs.recommended,
+  configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  pluginImportX.flatConfigs.recommended,
+  pluginImportX.flatConfigs.typescript,
+  jsxAlly.flatConfigs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    languageOptions: {
+      globals: jest.environments.globals.globals,
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
       },
-      plugins: {
-        'react-hooks': reactHooks,
-        'react-refresh': reactRefresh,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      'import-x/no-dynamic-require': 'warn',
+      'import-x/no-nodejs-modules': 'warn',
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+      'testing-library/await-async-queries': 'error',
+      'testing-library/no-await-sync-queries': 'error',
+      'testing-library/no-debugging-utils': 'warn',
+      'testing-library/no-dom-import': 'off',
+      ...vitest.configs.recommended.rules,
+      'vitest/max-nested-describe': ['error', { max: 3 }],
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect',
       },
-      rules: {
-        ...reactHooks.configs.recommended.rules,
-        'react-refresh/only-export-components': [
-          'warn',
-          { allowConstantExport: true },
-        ],
+      'import/resolver': {
+        typescript: {
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
     },
-  )
-  .concat(eslintPluginPrettier);
+  },
+  {
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/prefer-stateless-function': 'error',
+      //'react/button-has-type': 'error',
+      'react/no-unused-prop-types': 'error',
+      'react/jsx-pascal-case': 'error',
+      'react/jsx-no-script-url': 'error',
+      'react/no-children-prop': 'error',
+      'react/no-danger': 'error',
+      'react/no-danger-with-children': 'error',
+      'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+      'react/jsx-fragments': 'error',
+      'react/destructuring-assignment': [
+        'error',
+        'always',
+        { destructureInSignature: 'always' },
+      ],
+      'react/jsx-no-leaked-render': ['error', { validStrategies: ['ternary'] }],
+      'react/jsx-max-depth': ['error', { max: 5 }],
+      'react/function-component-definition': [
+        'warn',
+        { namedComponents: 'arrow-function' },
+      ],
+      'react/jsx-key': [
+        'error',
+        {
+          checkFragmentShorthand: true,
+          checkKeyMustBeforeSpread: true,
+          warnOnDuplicates: true,
+        },
+      ],
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/jsx-curly-brace-presence': 'warn',
+      'react/no-typos': 'warn',
+      'react/display-name': 'warn',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-sort-props': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-one-expression-per-line': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'default',
+          format: ['PascalCase', 'camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'variable',
+          // Specify PascalCase for React components
+          format: ['PascalCase', 'camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'parameter',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'property',
+          format: null,
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+      ],
+
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        { assertionStyle: 'never' },
+      ],
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/member-ordering': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+    },
+  },
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import viteLogo from '../public/vite.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img alt="Vite logo" className="logo" src={viteLogo} />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
+        <button
+          onClick={() => {
+            setCount(count => count + 1);
+          }}
+        >
           count is {count}
         </button>
         <p>
@@ -30,6 +33,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
